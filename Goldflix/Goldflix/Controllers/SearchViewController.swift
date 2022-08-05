343bed3700cd96a7d5570fcf19e6d67ca0456814
Yeskendir <91181514+goldmangrey@users.noreply.github.com>
2022-08-05 12:22:16 +0600

    Add files via upload

---
+++ Goldflix/Goldflix/Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Goldflix
+//
+//  Created by Yeskendir on 22.06.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+   
+}
